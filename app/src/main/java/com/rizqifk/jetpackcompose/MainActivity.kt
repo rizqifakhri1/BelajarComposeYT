f64d100d86ec24b283d8aaf2397e6c07dff5434c
rizqifakhri1 <rizqifakhri@gmail.com>
2022-06-09 13:00:46 +0700

    Initial commit

---
+++ app/src/main/java/com/rizqifk/jetpackcompose/MainActivity.kt
@@ -0,0 +1,103 @@
+ package com.rizqifk.jetpackcompose
+
+import android.os.Bundle
+import android.os.ParcelFileDescriptor
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.rizqifk.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val painter = painterResource(id = R.drawable.face)
+            val description = "Ini muka orang keren banget"
+            val title = "Mahasiswa Keren"
+            Box(modifier = Modifier
+                .fillMaxSize(0.5f)
+                .padding(16.dp)
+            ){
+                ImageCard(painter = painter, contentDescriptor = description, title = title)
+            }
+
+        }
+    }
+}
+
+@Composable
+ fun ImageCard(
+    painter: Painter,
+    contentDescriptor: String,
+    title: String,
+    modifier: Modifier = Modifier
+ ){
+     Card(
+         modifier = modifier.fillMaxWidth(),
+         shape = RoundedCornerShape(16.dp),
+         elevation = 5.dp
+     ) {
+         Box(modifier = Modifier.height(200.dp)){
+             Image( // Image
+                 painter = painter,
+                 contentDescription = contentDescriptor,
+                 contentScale = ContentScale.Crop //centercrop
+             )
+             Box(modifier = Modifier //Membuat Gradasi
+                 .fillMaxSize()
+                 .background(
+                     Brush.verticalGradient(
+                         colors = listOf(
+                             Color.Transparent,
+                             Color.Black
+                         ),
+                         startY = 300f //Tingkat gradasi mau dari mana
+                     )
+                 )
+             )
+             Box( //Untuk Title
+                 modifier = Modifier
+                     .fillMaxSize()
+                     .padding(12.dp),
+                 contentAlignment = Alignment.BottomStart
+             ){
+                 Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+             }
+         }
+     }
+ }
+/*
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposeTheme {
+        Greeting("Android")
+    }
+}*/
